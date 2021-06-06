50b2c0b7518478a4fb76de9add7256d2e465ba7d
Luciano Nicolas Prina <luciano.prina@hotmail.es>
2021-06-05 22:03:32 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calc-trade';
+  valoresTotales: string[] = []
+
+  constructor(
+  ){
+    // this.buildForm();
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  calculator(){
+    this.valoresTotales = [];
+    let valor = parseInt(((<HTMLInputElement>document.getElementById('valor')).value));
+    let porcentaje: number = parseInt(((<HTMLInputElement>document.getElementById('porcentaje')).value));
+    let operaciones: number = parseInt(((<HTMLInputElement>document.getElementById('operaciones')).value));
+
+    
+    for (let i = 0; i < operaciones; i++){
+      valor = valor*((porcentaje/100)+1);
+      this.valoresTotales.push(valor.toString());
+    }
+  }
+
+  /* buildForm(){
+    this.form = this.formBuilder.group({
+      valor: [''],
+      porcentaje: [''],
+      operaciones: [''],
+    })
+  } */
+
+
+
 }
