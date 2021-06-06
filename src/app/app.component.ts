fa4becbbbec417cb8e144b080272f80285c23b8d
Luciano Nicolas Prina <luciano.prina@hotmail.es>
2021-06-06 18:02:51 -0300

    3th commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,15 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  valoresTotales: string[] = []
+  valoresTotales: any[] = [];
+  valoresTotalesUSD: any[] = [];
+  gananciasTotales: any[] = [];
+  gananciasDiarias: any[] = [];
+  valor!: number;
+  aux!: number;
 
   constructor(
-  ){
+  ) {
     // this.buildForm();
 
   }
@@ -19,27 +24,28 @@ export class AppComponent {
 
   }
 
-  calculator(){
+  calculator() {
     this.valoresTotales = [];
-    let valor = parseInt(((<HTMLInputElement>document.getElementById('valor')).value));
+    this.valoresTotalesUSD = [];
+    this.gananciasTotales = [];
+    this.gananciasDiarias = [];
+    this.valor = parseInt(((<HTMLInputElement>document.getElementById('valor')).value));
     let porcentaje: number = parseInt(((<HTMLInputElement>document.getElementById('porcentaje')).value));
     let operaciones: number = parseInt(((<HTMLInputElement>document.getElementById('operaciones')).value));
+    this.aux = this.valor;
 
-    
-    for (let i = 0; i < operaciones; i++){
-      valor = valor*((porcentaje/100)+1);
-      this.valoresTotales.push(valor.toString());
+    for (let i = 0; i < operaciones; i++) {
+      this.valor = this.valor * ((porcentaje / 100) + 1);
+      this.valoresTotales.push(this.valor);
+      let aux4 = (this.valor / 164);
+      this.valoresTotalesUSD.push(aux4);
+      this.gananciasTotales.push((this.valor - this.aux));
+      console.log(this.valor)
+      let aux2 = this.valor;
+      if (aux2) {
+        this.gananciasDiarias.push((aux2 - this.valoresTotales[i - 1]));
+      }
     }
   }
-
-  /* buildForm(){
-    this.form = this.formBuilder.group({
-      valor: [''],
-      porcentaje: [''],
-      operaciones: [''],
-    })
-  } */
-
-
 
 }
